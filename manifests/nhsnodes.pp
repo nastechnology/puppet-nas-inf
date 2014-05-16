3c4cf37bf3c20246a4b80b234a4b1645b5351a97
Mark Myers <marcusmyers@gmail.com>
2014-05-16 16:24:30 -0400

    Enabled backup for Lindy Seagrave

--- manifests/nhsnodes.pp
+++ manifests/nhsnodes.pp
@@ -83,17 +83,12 @@ node 'nhs-mac-06176.nas.local' inherits 'teachersmacnode' {
 
 # Lindy Seagrave
 # backup user created
-node 'nhs-mac-06182.nas.local' inherits 'teachersmacnode' {
-  class { 'nacs_management::allprinters': }
-  nacs_management::map::idrive { '1010009': }
-  nacs_management::printers{ 'nhs_wkrm_copier': }
-  nacs_management::printers{ 'nhs_wkrm_copier_2': }
-  nacs_management::printers{ 'nhs_wkrm_231': }
-  nacs_management::printers{ 'nhs_library': }
-  nacs_management::printers{ 'nhs_207': }
+node 'nhs-mac-06182.nas.local' inherits {
+  $user = '1010009'
 
-  nacs_management::map::alldistrict { '1010009': }
-  nacs_management::map::nhsshared { '1010009': }
+  class { 'roles::teacher::nhs':
+    user => "${user}",
+  }
 }
 
 # Seth Izor Mac
