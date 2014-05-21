5f11f7567438eca659590522b3afd348f7b6d283
Mark Myers <marcusmyers@gmail.com>
2014-05-21 09:25:06 -0400

    Added Peg Mossing to backup

--- manifests/nmsnodes.pp
+++ manifests/nmsnodes.pp
@@ -409,16 +409,12 @@ node 'nms-mac-06085.nas.local' inherits 'teachersmacnode' {
 
 # Peggy Mossing Mac
 # backup user created
-node 'nms-mac-06086.nas.local' inherits 'teachersmacnode' {
-  class { 'nacs_management::allprinters': }
-  nacs_management::map::idrive { '1005642': }
-  nacs_management::printers { 'nms_upstairs_copier': }
-  nacs_management::printers { 'nms_office': }
-  nacs_management::printers { 'nms_office_copier': }
-  nacs_management::printers{ 'ces_upstairs_wkrm': }
+node 'nms-mac-06086.nas.local' {
+  $user = '1005642'
 
-  nacs_management::map::nmsshared { '1005642':  }
-  nacs_management::map::alldistrict { '1005642': }
+  class { 'roles::teacher::nms':
+    user => $user,
+  }
 }
 
 # Cara Ressler Mac
