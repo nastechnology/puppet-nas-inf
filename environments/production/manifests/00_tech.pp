11e9145a479cf21ac42c3d4acb6b0d51340ce87a
Mark Myers <marcusmyers@gmail.com>
2014-06-12 10:58:38 -0400

    Updated tech nodes

--- environments/production/manifests/00_tech.pp
+++ environments/production/manifests/00_tech.pp
@@ -1,35 +1,17 @@
 # Marks MacBook Pro
-node 'nas-tech-tc.nas.local' inherits 'staffmacnode' {
-  class { 'nacs_management::allprinters': }
-  nacs_management::printers { 'boe_copier': }
-  nacs_management::printers { 'ces_wkrm': }
-  nacs_management::printers { 'ces_upstairs_wkrm': }
+node 'nas-tech-tc.nas.local' {
+  $user = 'markmyers'
 
-  nacs_management::printers { 'cdb_wkrm_copier': }
-  nacs_management::printers { 'hs_stafflounge': }
-
-  include nacs_management::checkin
-
-  printer { 'NHS_WKRM_COPIER1':
-    ensure      => present,
-    uri         => 'ipp://10.20.2.9/printers/NHS_WKRM_COPIER1',
-    description => 'NHS WKRM Copier1',
-    location    => 'NHS 231',
-    shared      => false,
+  class { 'roles::staff::tech':
+    user => $user,
   }
 }
 
 # Chris MacBook Professional
-node 'nas-tech-st.nas.local' inherits 'staffmacnode' {
-  class { 'nacs_management::allprinters': }
-  nacs_management::printers { 'boe_copier': }
+node 'nas-tech-st.nas.local' {
+  $user = 'crwenner'
 
-  nacs_management::printers { 'wes_wkrm_copier': }
-
-  nacs_management::printers { 'nhs_wkrm_copier': }
-  nacs_management::printers { 'nhs_wkrm_copier_2': }
-
-  nacs_management::printers { 'nms_office_copier': }
-
-  nacs_management::printers { 'ces_office_copier': }
+  class { 'roles::staff::tech':
+    user => $user,
+  }
 }
