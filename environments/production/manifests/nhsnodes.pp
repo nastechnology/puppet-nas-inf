f624204508158f6664b9f443dc26beaaf0e7f79c
Mark Myers <marcusmyers@gmail.com>
2014-07-09 09:11:21 -0400

    Added VMWare View to Ryans laptop

--- environments/production/manifests/nhsnodes.pp
+++ environments/production/manifests/nhsnodes.pp
@@ -35,6 +35,12 @@ node 'nhs-mac-06183.nas.local' {
 # Ryan Wilde Mac
 node 'nhs-mac-06307.nas.local' {
   include roles::staff::nhs
+
+  package { 'VMware Horizon View Client 3.0.0-1887158':
+    ensure   => installed,
+    provider => 'pkgdmg',
+    source   => 'http://tech.napoleonareaschools.org/wp-content/uploads/2014/07/VMware_Horizon_View_Client-3.0.0-1887158.dmg',
+  }
   #$version = '4.3.12'
   #$patch_level = '93733'
 
