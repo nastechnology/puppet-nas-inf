dd35d99565a14525e39328d9c3bcef0d5d23b9fa
Mark Myers <marcusmyers@gmail.com>
2014-07-31 16:00:22 -0400

    Added all printers drivers

--- environments/production/manifests/nctv.pp
+++ environments/production/manifests/nctv.pp
@@ -1,33 +1,41 @@
 node 'nctv-lap3.nas.local' inherits 'macbasenode' {
+  class { 'nacs_management::allprinters': }
   nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-mini2.nas.local' inherits 'macbasenode' {
+  class { 'nacs_management::allprinters': }
   nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-mini1.nas.local' inherits 'macbasenode' {
+  class { 'nacs_management::allprinters': }
   nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-pro1.nas.local' inherits 'macbasenode' {
+  class { 'nacs_management::allprinters': }
   nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-pro2.nas.local' inherits 'macbasenode' {
+  class { 'nacs_management::allprinters': }
   nacs_management::printers { 'nhs_copier': }
 }
 
 
 node 'nctv-lap2.nas.local' inherits 'macbasenode' {
+  class { 'nacs_management::allprinters': }
   nacs_management::printers { 'nhs_copier': }
 }
 
 
 node 'nctv-lap4.nas.local' inherits 'macbasenode' {
+  class { 'nacs_management::allprinters': }
   nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-lap5.nas.local' inherits 'macbasenode' {
+  class { 'nacs_management::allprinters': }
   nacs_management::printers { 'nhs_copier': }
 }
