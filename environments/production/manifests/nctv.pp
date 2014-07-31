7b2ba4c6d3b76e7f076a2cc985a5187ffad86946
Mark Myers <marcusmyers@gmail.com>
2014-07-31 15:51:28 -0400

    Moved to new printer definition

--- environments/production/manifests/nctv.pp
+++ environments/production/manifests/nctv.pp
@@ -1,33 +1,33 @@
 node 'nctv-lap3.nas.local' inherits 'macbasenode' {
-  include nacs_management::install_printers::nhs_copier
+  nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-mini2.nas.local' inherits 'macbasenode' {
-  include nacs_management::install_printers::nhs_copier
+  nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-mini1.nas.local' inherits 'macbasenode' {
-  include nacs_management::install_printers::nhs_copier
+  nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-pro1.nas.local' inherits 'macbasenode' {
-  include nacs_management::install_printers::nhs_copier
+  nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-pro2.nas.local' inherits 'macbasenode' {
-  include nacs_management::install_printers::nhs_copier
+  nacs_management::printers { 'nhs_copier': }
 }
 
 
 node 'nctv-lap2.nas.local' inherits 'macbasenode' {
-  include nacs_management::install_printers::nhs_copier
+  nacs_management::printers { 'nhs_copier': }
 }
 
 
 node 'nctv-lap4.nas.local' inherits 'macbasenode' {
-  include nacs_management::install_printers::nhs_copier
+  nacs_management::printers { 'nhs_copier': }
 }
 
 node 'nctv-lap5.nas.local' inherits 'macbasenode' {
-  include nacs_management::install_printers::nhs_copier
+  nacs_management::printers { 'nhs_copier': }
 }
